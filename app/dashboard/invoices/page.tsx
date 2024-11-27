f4878ac99fa3014666e891ccb7bcad6f5421a1da
Nseigou <nagano_1si@yahoo.co.jp>
2024-11-27 17:17:23 +0900

    mk

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,39 @@
-export default function Invoices() {
-    return <p>Invoices Page</p>;
+import Pagination from '@/app/ui/invoices/pagination';
+import Search from '@/app/ui/search';
+import Table from '@/app/ui/invoices/table';
+import { CreateInvoice } from '@/app/ui/invoices/buttons';
+import { lusitana } from '@/app/ui/fonts';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
+import { fetchInvoicesPages } from '@/app/lib/data';
+ 
+export default async function Page({ 
+    searchParams,
+} : {
+    searchParams?:{  //?はオプショナルチェイニング演算子なのでsearchParamsが存在しなくてもエラーにならない
+        query?: string;
+        page?: string;
+    };
+}) {
+    const query = searchParams?.query || ''; //searchParamsが存在すればqueryを取得、なければ空文字を返す
+    const currentPage = Number(searchParams?.page) || 1; //searchParamsが存在すればpageを取得、なければ1を返す
+
+    const totalPages = await fetchInvoicesPages(query);
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search invoices..." />
+        <CreateInvoice />
+      </div>
+       <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <Table query={query} currentPage={currentPage} />
+      </Suspense>
+      <div className="mt-5 flex w-full justify-center">
+        <Pagination totalPages={totalPages} />
+      </div>
+    </div>
+  );
 }
